ce48e843d09c07199654e4eef642751ef5d8718d
Subhash Suryawanshi <44070827+sunle-af@users.noreply.github.com>
2021-01-06 11:20:04 +0530

    uploaded

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import './App.css';
+import Navg from "./components/Navbar/Navg";
+function App() {
+  
+
+  return (
+    <> 
+     <div   class="Head" className="App">
+      
+     </div>
+     
+    <div class= "box">
+    <Navg />
+    <div class="hero1">
+    <h1 class="text">BORED OR EXHAUTED</h1>
+    </div>
+    <div class="hero2">
+     <h1 class="text">SAD OR HAPPY</h1>
+    </div>
+    <div class="hero3">
+    <h1 class="text">GRAB A COFFEE!</h1>
+    </div>
+    <div class="hero4">
+      <h1 class='txt'>EXPLORE</h1>
+    </div>
+   
+    
+      
+    </div>
+    
+      </>
+  );
+
+}
+
+export default App;
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'HOME',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'ABOUT US',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'PRODUCTS',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'GET A QUBI',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'CONTACT US',
+        url: '#',
+        cName: 'nav-links'
+    }
+
+]
